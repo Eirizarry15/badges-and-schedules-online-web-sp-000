5e266d4aac52b5f27c154bfb2e165f3a062598d0
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-10 19:39:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,27 @@ def batch_badge_creator(speakers)
   end
   badges
 end
+
+def assign_rooms(speakers)
+  room = 1 
+  room_message = []
+  speakers.each do |speaker|
+    room_message << "Hello, #{speaker}! You'll be assigned to room #{room}!"
+    room += 1 
+  end
+  room_message 
+end 
+  
+ def printer(speakers)
+   badges = batch_badge_creator(speakers)
+   badges.each do |message|
+     puts message 
+    end 
+    room_message = assign_rooms(speakers)
+    room_message.each do |message|
+      puts message
+    end 
+  
+  
+  
+end
