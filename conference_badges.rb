792d21c0d7be34c49d5910f1f91e483722832be8
Edgar Irizarry <eirizarry15@yahoo.com>
2018-10-10 19:34:00 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}"
+end
+
+def batch_badge_creator(speakers)
+  badges = []
+  speakers.each do |speaker|
+    message == badges(speaker)
+    badges << message
+  end
+  badges
+end
